639de5bd740448ab6d8d24a50ed7a1495f737b5c
JoelP-11 <Jp421513@gmail.com>
2023-05-14 01:06:09 -0700

    I think i'm close to finishing but I got stumped so I will be doing more frequent pushes to be safe.

--- README.md
+++ README.md
@@ -1,9 +1,53 @@
-# coo app
-
+# cool app
+  ![](https://img.shields.io/badge/license-MIT-blue.svg)
+  
   ## Description
 
-  its cool
+  it is cool
 
-  #{renderLicenseBadge(data.license)}
+  ## Table Of Contents
 
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+  
+* [license](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+  ```
+  undefined
+  ```
+
+  ## Usage
+
+  undefined
+
+  ## License
+    
+    This project is licensed under the MIT license.
+
+  ## Contributing
+
+  undefined
+
+  ## Tests
+
+  To run tests, run the following command:
+
+  ```
+  undefined
+  ```
+
+  ## Questions
+
+  If you have any questions about the repo, open an issue or contact me directly at undefined. You can find more of my work at [undefined](https://github.com/undefined/).
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
@@ -8,7 +9,7 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the name of your app'
+        message: 'What is the title of your project?'
     },
     {
     type: 'input',
@@ -20,11 +21,23 @@ const questions = [
     name: 'license',
     message: 'What license does this repo have?',
     choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-}
+},
+{
+    type: 'input',
+    name: 'questions',
+    message: 'What is your GitHub username?'
+},
+{
+    type: 'input',
+    name: 'questions',
+    message: 'What is your email?'
+},
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -32,7 +45,7 @@ function init() {
     .prompt(questions)
     .then(answers => {
         console.log(answers);
-        fs.writeFile('README.md', generateMarkdown(answers), err => {
+        writeToFile('README.md', generateMarkdown(answers), err => {
             if (err) return console.error(err);
 
             console.log('README generated successfully');
